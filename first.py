62e29a2a242cab698ce26b0bbb3a933117a39d15
ydyryselmira7979-commits <ydyrys.elmira7979@gmail.com>
2025-10-16 13:59:54 +0200

    Add files via upload

---
+++ first.py
@@ -0,0 +1,10 @@
+def sudy_nebo_lichy(cislo):
+    """
+    Upravte funkci tak, aby vypisovala, zda je cislo sude nebo liche
+    """
+    print(f"Cislo {cislo} je sude")
+
+
+if __name__ == "__main__":
+    sudy_nebo_lichy(5)
+    sudy_nebo_lichy(1000000)
